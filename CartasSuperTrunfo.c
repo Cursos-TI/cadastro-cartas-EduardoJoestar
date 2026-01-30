053258aa24aa242e9fe87cefaade4d853dea1d89
Eduardo Marinho <cn72169@gmail.com>
2026-01-30 00:24:51 +0000

    FIZ O CODIGO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,43 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int população, pontos_turisticos;
+  char codigo[50];
+  float area, pib;
 
   // Área para entrada de dados
-
+  printf("Bem Vindo ao Super Trunfo - Cidades!\n");
+  printf("Vamos cadastrar a primeira cidade. \n");
+  printf("Digite o código da cidade: ");
+  scanf(" %s", codigo);
+ printf("Declare a População, Área, PIB e Pontos Turísticos\n");
+  scanf("%d %f %f %d", &população, &area, &pib, &pontos_turisticos);
   // Área para exibição dos dados da cidade
+  printf("Carta 1: \n");
+  printf("Cidade: %s\n", codigo);
+  printf("População: %d\n", população);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: R$ %.2f bilhões\n", pib);
+  printf("Pontos Turísticos: %d\n", pontos_turisticos);
+
+  printf("Cidade Cadastrada com Sucesso! \n");
+  printf("Cadastre a próxima cidade. \n");
+
+   printf("Digite o código da cidade: ");
+  scanf(" %s", codigo);
+  printf("Declare a População, Área, PIB e Pontos Turísticos\n");
+  scanf("%d %f %f %d", &população, &area, &pib, &pontos_turisticos);
+  // Área para exibição dos dados da cidade
+  printf("Carta 2: \n");
+  printf("Cidade: %s\n", codigo);
+  printf("População: %d\n", população);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: R$ %.2f bilhões\n", pib);
+  printf("Pontos Turísticos: %d\n", pontos_turisticos);
+
+  printf("Cidade Cadastrada com Sucesso! \n");
+  printf("Obrigado por jogar o Super Trunfo - Cidades!\n");
+
 
 return 0;
 } 
